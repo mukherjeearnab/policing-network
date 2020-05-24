485bf68ec99f939333b9a8f0836c38f9c835a615
Arnab Mukherjee <35556016+mukherjeearnab@users.noreply.github.com>
2020-05-24 17:10:24 +0530

    fixef bugs => ccp-generate.sh

--- network-config/scripts/ccp-generate.sh
+++ network-config/scripts/ccp-generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function one_line_pem() {
-    echo "$(awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1)"
+function one_line_pem {
+    echo "`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1`"
 }
 
-function json_ccp() {
+function json_ccp {
     local PP=$(one_line_pem $5)
     local CP=$(one_line_pem $6)
     sed -e "s/\${ORG}/$1/" \
@@ -13,8 +13,9 @@ function json_ccp() {
         -e "s/\${CAPORT}/$4/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        ../../connections/ccp-template.json
+        ../../connections/ccp-template.json 
 }
+
 
 ORG=citizen
 ORGMSP=Citizen
@@ -23,7 +24,7 @@ CAPORT=7054
 PEERPEM=../crypto-config/peerOrganizations/citizen.example.com/tlsca/tlsca.citizen.example.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/citizen.example.com/ca/ca.citizen.example.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-citizen.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-citizen.json
 
 ORG=police
 ORGMSP=Police
@@ -32,8 +33,7 @@ CAPORT=8054
 PEERPEM=../crypto-config/peerOrganizations/police.example.com/tlsca/tlsca.police.example.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/police.example.com/ca/ca.police.example.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-police.json
-
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-police.json
 ORG=forensics
 ORGMSP=Forensics
 P0PORT=9051
@@ -41,7 +41,7 @@ CAPORT=9054
 PEERPEM=../crypto-config/peerOrganizations/forensics.example.com/tlsca/tlsca.forensics.example.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/forensics.example.com/ca/ca.forensics.example.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-forensics.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-forensics.json
 
 ORG=court
 ORGMSP=Court
@@ -50,7 +50,7 @@ CAPORT=10054
 PEERPEM=../crypto-config/peerOrganizations/court.example.com/tlsca/tlsca.court.example.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/court.example.com/ca/ca.court.example.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-court.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-court.json
 
 ORG=identityprovider
 ORGMSP=IdentityProvider
@@ -59,4 +59,4 @@ CAPORT=11054
 PEERPEM=../crypto-config/peerOrganizations/identityprovider.example.com/tlsca/tlsca.identityprovider.example.com-cert.pem
 CAPEM=../crypto-config/peerOrganizations/identityprovider.example.com/ca/ca.identityprovider.example.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" >../../connections/connection-identityprovider.json
+echo "$(json_ccp $ORG $ORGMSP $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../../connections/connection-identityprovider.json
