4e7af452221bafda3b1f7030b1b3d4a036f0610d
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-05-27 00:54:46 +0530

    added evidence_cc

--- chaincode/evidence_cc/chaincode.go
+++ chaincode/evidence_cc/chaincode.go
@@ -3,20 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
+	"github.com/hyperledger/fabric/common/util"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
+	"strconv"
 )
 
 // Chaincode is the definition of the chaincode structure.
 type Chaincode struct {
 }
 
-// Definition of the Asset structure
-type asset struct {
-	ID    string `json:"objID"`
-	Name  string `json:"objName"`
-	Owner string `json:"objOwner"`
+// Definition of the Evidence structure
+type evidence struct {
+	Type            string `json:"Type"`
+	ID              string `json:"ID"`
+	Description     string `json:"Description"`
+	DateTime        int    `json:"DateTime"`
+	InvestigationID string `json:"InvestigationID"`
 }
 
 // Init is called when the chaincode is instantiated by the blockchain network.
@@ -29,65 +32,76 @@ func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) sc.Response {
 	fcn, params := stub.GetFunctionAndParameters()
 	fmt.Println("Invoke()", fcn, params)
 
-	if fcn == "createAsset" {
-		return cc.createAsset(stub, params)
-	} else if fcn == "readAsset" {
-		return cc.readAsset(stub, params)
-	} else if fcn == "updateAsset" {
-		return cc.updateAsset(stub, params)
-	} else if fcn == "deleteAsset" {
-		return cc.deleteAsset(stub, params)
+	if fcn == "addEvidence" {
+		return cc.addEvidence(stub, params)
+	} else if fcn == "readEvidence" {
+		return cc.readEvidence(stub, params)
 	} else {
 		fmt.Println("Invoke() did not find func: " + fcn)
 		return shim.Error("Received unknown function invocation!")
 	}
 }
 
-// Function to create new asset (C of CRUD)
-func (cc *Chaincode) createAsset(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+// Function to add new Evidence
+func (cc *Chaincode) addEvidence(stub shim.ChaincodeStubInterface, params []string) sc.Response {
 	// Check if sufficient Params passed
-	if len(params) != 3 {
+	if len(params) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
 	// Check if Params are non-empty
-	if len(params[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
-	}
-	if len(params[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string")
-	}
-	if len(params[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string")
+	for a := 0; a < 4; a++ {
+		if len(params[a]) <= 0 {
+			return shim.Error("Argument must be a non-empty string")
+		}
 	}
 
-	// Check if Asset exists with Key => params[0]
-	assetAsBytes, err := stub.GetState(params[0])
+	ID := params[0]
+	Description := params[1]
+	DateTime := params[2]
+	InvestigationID := params[3]
+	DateTimeI, err := strconv.Atoi(DateTime)
 	if err != nil {
-		return shim.Error("Failed to check if Asset exists!")
-	} else if assetAsBytes != nil {
-		return shim.Error("Asset Already Exists!")
+		return shim.Error("Error: Invalid DateTime!")
 	}
 
-	// Generate Asset from params provided
-	asset := &asset{params[0], params[1], params[2]}
-	assetJSONasBytes, err := json.Marshal(asset)
+	// Check if Evidence exists with Key => params[0]
+	evidenceAsBytes, err := stub.GetState(ID)
+	if err != nil {
+		return shim.Error("Failed to check if Evidence exists!")
+	} else if evidenceAsBytes != nil {
+		return shim.Error("Evidence Already Exists!")
+	}
+
+	// Generate Evidence from params provided
+	evidence := &evidence{"evidence",
+		ID, Description, DateTimeI, InvestigationID}
+
+	// Get JSON bytes of Evidence struct
+	evidenceJSONasBytes, err := json.Marshal(evidence)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	// Put State of newly generated Asset with Key => params[0]
-	err = stub.PutState(params[0], assetJSONasBytes)
+	// Put State of newly generated Evidence with Key => params[0]
+	err = stub.PutState(ID, evidenceJSONasBytes)
 	if err != nil {
 		return shim.Error(err.Error())
+	}
+
+	// Add Evidence ID to The Investigation with InvestigationID
+	args := util.ToChaincodeArgs("addEvidence", InvestigationID, ID)
+	response := stub.InvokeChaincode("investigation_cc", args, "mainchannel")
+	if response.Status != shim.OK {
+		return shim.Error(response.Message)
 	}
 
 	// Returned on successful execution of the function
 	return shim.Success(nil)
 }
 
-// Function to read an asset (R of CRUD)
-func (cc *Chaincode) readAsset(stub shim.ChaincodeStubInterface, params []string) sc.Response {
+// Function to read an Evidence
+func (cc *Chaincode) readEvidence(stub shim.ChaincodeStubInterface, params []string) sc.Response {
 	// Check if sufficient Params passed
 	if len(params) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
@@ -98,89 +112,16 @@ func (cc *Chaincode) readAsset(stub shim.ChaincodeStubInterface, params []string
 		return shim.Error("1st argument must be a non-empty string")
 	}
 
-	// Get State of Asset with Key => params[0]
-	assetAsBytes, err := stub.GetState(params[0])
+	// Get State of Evidence with Key => params[0]
+	evidenceAsBytes, err := stub.GetState(params[0])
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to get state for " + params[0] + "\"}"
 		return shim.Error(jsonResp)
-	} else if assetAsBytes == nil {
-		jsonResp := "{\"Error\":\"Asset does not exist!\"}"
+	} else if evidenceAsBytes == nil {
+		jsonResp := "{\"Error\":\"Evidence does not exist!\"}"
 		return shim.Error(jsonResp)
 	}
 
 	// Returned on successful execution of the function
-	return shim.Success(assetAsBytes)
-}
-
-// Function to update an asset's owner (U of CRUD)
-func (cc *Chaincode) updateAsset(stub shim.ChaincodeStubInterface, params []string) sc.Response {
-	// Check if sufficient Params passed
-	if len(params) != 2 {
-		return shim.Error("Incorrect number of arguments. Expecting 2")
-	}
-
-	// Check if Params are non-empty
-	if len(params[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
-	}
-	if len(params[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string")
-	}
-
-	// Get State of Asset with Key => params[0]
-	assetAsBytes, err := stub.GetState(params[0])
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + params[0] + "\"}"
-		return shim.Error(jsonResp)
-	} else if assetAsBytes == nil {
-		jsonResp := "{\"Error\":\"Asset does not exist!\"}"
-		return shim.Error(jsonResp)
-	}
-
-	// Create new Asset Variable
-	assetToTransfer := asset{}
-	err = json.Unmarshal(assetAsBytes, &assetToTransfer) //unmarshal it aka JSON.parse()
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	// Update asset.Owner => params[1]
-	assetToTransfer.Owner = params[1]
-
-	// Convert to Byte[]
-	assetJSONasBytes, err := json.Marshal(assetToTransfer)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	// Put updated State of the Asset with Key => params[0]
-	err = stub.PutState(params[0], assetJSONasBytes)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	// Returned on successful execution of the function
-	return shim.Success(nil)
-}
-
-// Function to Delete an asset (D of CRUD)
-func (cc *Chaincode) deleteAsset(stub shim.ChaincodeStubInterface, params []string) sc.Response {
-	// Check if sufficient Params passed
-	if len(params) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 2")
-	}
-
-	// Check if Params are non-empty
-	if len(params[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string")
-	}
-
-	// Delete the State with Key => params[0]
-	err := stub.DelState(params[0])
-	if err != nil {
-		return shim.Error("Failed to delete Asset: " + err.Error())
-	}
-
-	// Returned on successful execution of the function
-	return shim.Success(nil)
+	return shim.Success(evidenceAsBytes)
 }
