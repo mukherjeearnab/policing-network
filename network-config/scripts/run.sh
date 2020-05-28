3b1fe52fb952aaf6738f6bed9e1b76b583248e81
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-05-28 14:11:46 +0530

    resolved bug in run.sh

--- network-config/scripts/run.sh
+++ network-config/scripts/run.sh
@@ -10,14 +10,14 @@ docker exec -it cli bash ./scripts/channel/join-peer.sh peer0 identityprovider I
 
 CC_NAMES="chargesheet_cc citizenprofile_cc evidence_cc fir_cc investigation_cc judgement_cc"
 
-for CC in CC_NAMES; do
-    echo "Installing ".$CC
+for CC in $CC_NAMES; do
+    echo "Installing "$CC
     docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 citizen CitizenMSP 7051 1.0
     docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 police PoliceMSP 8051 1.0
     docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 forensics ForensicsMSP 9051 1.0
     docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 court CourtMSP 10051 1.0
     docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 identityprovider IdentityProviderMSP 11051 1.0
-    echo "Instantiating ".$CC
+    echo "Instantiating "$CC
     docker exec -it cli bash ./scripts/install-cc/instantiate.sh $CC
 done
 
