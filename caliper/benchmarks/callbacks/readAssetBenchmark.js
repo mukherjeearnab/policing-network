8e3b9513f908f9fea2c5a726fb51c8dcb3639b46
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2020-12-16 16:46:10 +0000

    Update Caliper Workspace to latest WORKING changes

--- caliper/benchmarks/callbacks/readAssetBenchmark.js
+++ caliper/benchmarks/callbacks/readAssetBenchmark.js
@@ -1,8 +1,8 @@
 "use strict";
 
-module.exports.info = "Create and Read Evidences";
+module.exports.info = "Create and Read FIR";
 
-const contractID = "evidence_cc";
+const contractID = "fir_cc";
 const version = "1.0";
 
 let bc, ctx, clientArgs, clientIdx;
@@ -14,12 +14,24 @@ module.exports.init = async function (blockchain, context, args) {
     clientIdx = context.clientIdx.toString();
     for (let i = 0; i < clientArgs.assets; i++) {
         try {
-            const assetID = `${clientIdx}_${i}`;
-            console.log(`Client ${clientIdx}: Creating evidence ${assetID}`);
+            const assetID = `FIR1_${clientIdx}_${i}`;
+            console.log(`Client ${clientIdx}: Creating FIR ${assetID}`);
             const myArgs = {
-                chaincodeFunction: "addEvidence",
-                invokerIdentity: "Admin@org1.example.com",
-                chaincodeArguments: [assetID, "jpeg", ".jpg", "An image.", "10000", "af34bce1"],
+                chaincodeFunction: "createNewFIR",
+                invokerIdentity: "Admin@citizen.example.com",
+                chaincodeArguments: [
+                    assetID,
+                    "citizen1",
+                    "Bosnia",
+                    "ZZZ",
+                    "DUDEX",
+                    "1000",
+                    "XXX",
+                    "ZZZ",
+                    "XYZ",
+                    "XCZ",
+                    "XCV",
+                ],
             };
             await bc.bcObj.invokeSmartContract(ctx, contractID, version, myArgs);
         } catch (error) {
@@ -31,9 +43,9 @@ module.exports.init = async function (blockchain, context, args) {
 module.exports.run = function () {
     const randomId = Math.floor(Math.random() * clientArgs.assets);
     const myArgs = {
-        chaincodeFunction: "readEvidence",
-        invokerIdentity: "Admin@org1.example.com",
-        chaincodeArguments: [`${clientIdx}_${randomId}`],
+        chaincodeFunction: "readFIR",
+        invokerIdentity: "Admin@citizen.example.com",
+        chaincodeArguments: [`FIR1_${clientIdx}_${randomId}`],
     };
     return bc.bcObj.querySmartContract(ctx, contractID, version, myArgs);
 };
